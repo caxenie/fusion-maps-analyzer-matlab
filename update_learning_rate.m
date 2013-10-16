f7778089a5231fff275e32e04cb3a8798f606828
Cristian Axenie <cristian.axenie@gmail.com>
2013-10-16 20:00:51 +0200

    Added new update rules. Modularized code and sections.

--- update_learning_rate.m
+++ update_learning_rate.m
@@ -1,23 +1,21 @@
-function l = update_learning_rate(oldl, err, idx, ~, type)
+function l = update_learning_rate(oldl, err, idx, default_val, type)
 switch type
     case 'divisive'
         sum = 0;
-        default_val = 0.02;
         for k = 1:(length(err))
             if(k~=idx)
                 sum = sum + abs(err(k));
             end
         end
-        l = default_val*(sum/((length(err)-1)*abs(err(idx))));
+        l = default_val*(((length(err)-1)*abs(err(idx)))/sum);
     case 'decay'
         sum = 0;
-        default_val = 0.02;
         for k = 1:(length(err))
             if(k~=idx)
                 sum = sum + abs(err(k));
             end
         end
-        l = default_val*(sum/((length(err)-1)*abs(err(idx))));
-        l = oldl + l;   
+        l = default_val*(((length(err)-1)*abs(err(idx)))/sum);
+        l = oldl + l;
 end
 end
